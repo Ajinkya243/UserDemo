29b6fc9335b92db36b56946db8e99af3133783f2
Ajinkya243 <ajinkyagund2@gmail.com>
2025-07-24 12:03:59 +0530

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const {connectDB}=require("./config/db.connect");
 const express=require('express');
+const cors=require('cors');
 const app=express();
 require("dotenv").config();
 const port=process.env.PORT;
 const userRouter=require("./routes/user.routes");
 app.use(express.json());
+app.use(cors())
 
 app.get("/",async(req,res)=>{
     res.json({message:"User connected"})
