ac2fccb64a5f64f771882a95d64b11c27379fdda
Ajinkya243 <ajinkyagund2@gmail.com>
2025-07-24 11:23:30 +0530

    rearrange code

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const port=process.env.PORT;
 const userRouter=require("./routes/user.routes");
 app.use(express.json());
 
+app.get("/",async(req,res)=>{
+    res.json({message:"User connected"})
+})
+
+app.use("/api/user",userRouter);
 connectDB().then(()=>console.log("Database connected")).then(()=>{
     app.listen(port,async(req,res)=>{
         console.log("Express running port",port);
     })
 })
 
-app.get("/",async(req,res)=>{
-    res.json({message:"User connected"})
-})
-app.use("/api/user",userRouter);
+
